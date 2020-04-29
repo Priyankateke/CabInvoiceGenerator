b0603a3fa178280e8321e1ea183422218e06f3f3
Priyankateke <priiyankateke1705@gmail.com>
2020-04-29 08:22:18 -0700

    ADDED: Test case & Code To Calculate Total Fare

--- src/main/java/cabinvoice/InvoiceService.java
+++ src/main/java/cabinvoice/InvoiceService.java
@@ -1,4 +1,10 @@
 package cabinvoice;
 
 public class InvoiceService {
+    private static double RS_PER_KILOMETER = 10;
+    private static int RS_PER_MINUTE = 1;
+
+    public double calculateFare(double distance, int time) {
+        return RS_PER_KILOMETER * distance + RS_PER_MINUTE * time;
+    }
 }
--- src/test/java/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/cabinvoice/InvoiceServiceTest.java
@@ -1,5 +1,16 @@
 package cabinvoice;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class InvoiceServiceTest {
-    InvoiceService invoiceService=new InvoiceService();
+
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare() {
+        InvoiceService invoiceService=new InvoiceService();
+        double distance = 5.0;
+        int time = 5;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(55, fare, 0.0);
+    }
 }
