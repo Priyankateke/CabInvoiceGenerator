642b11f403aa9f40577e11a7cd437283dd0a2f0d
Priyankateke <priiyankateke1705@gmail.com>
2020-04-29 08:45:46 -0700

    ADDED: TDD To Return MinimumFare If TotalFare is Minimum

--- src/main/java/cabinvoice/InvoiceService.java
+++ src/main/java/cabinvoice/InvoiceService.java
@@ -3,8 +3,14 @@ package cabinvoice;
 public class InvoiceService {
     private static double RS_PER_KILOMETER = 10;
     private static int RS_PER_MINUTE = 1;
+    private static double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return RS_PER_KILOMETER * distance + RS_PER_MINUTE * time;
+        double totalFare = RS_PER_KILOMETER * distance + RS_PER_MINUTE * time;
+
+        //If TOTAL_FARE is less than MINIMUM_FARE should return MINIMUM_FARE
+        if(totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
--- src/test/java/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/cabinvoice/InvoiceServiceTest.java
@@ -1,16 +1,30 @@
 package cabinvoice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
 
+    InvoiceService invoiceService;
+    @Before
+    public void setUp() {
+        invoiceService=new InvoiceService();
+    }
+
     @Test
     public void givenDistanceAndTime_shouldReturnTotalFare() {
-        InvoiceService invoiceService=new InvoiceService();
         double distance = 5.0;
         int time = 5;
         double fare = invoiceService.calculateFare(distance, time);
-        Assert.assertEquals(55, fare, 0.0);
+        Assert.assertEquals(55.0, fare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_WhenTotalFareLessThanMinimumFare_shouldReturnMinimumFare() {
+        double distance = 0.3;
+        int time = 1;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(5.0, fare,0.0);
     }
 }
