27c4d0fbeab6565f6054596bf448c23c2289bed2
Priyankateke <priiyankateke1705@gmail.com>
2020-05-02 06:52:49 -0700

    ADDED: Use TDD To Pass Test Case Of Normal And Premium Fare Using Enum

--- src/main/java/cabinvoice/InvoiceService.java
+++ src/main/java/cabinvoice/InvoiceService.java
@@ -1,28 +1,18 @@
 package cabinvoice;
 
 public class InvoiceService {
-    private final double RS_PER_KILOMETER = 10;
-    private final int RS_PER_MINUTE = 1;
-    private final double MINIMUM_FARE = 5;
-    private RideRepository rideRepository;
 
-    public InvoiceService() {
-        this.rideRepository = new RideRepository();
-    }
+    RideRepository rideRepository = new RideRepository();
 
-    /** Calculating total fare*/
-    public double calculateFare(double distance, int time) {
-        double totalFare = RS_PER_KILOMETER * distance + RS_PER_MINUTE * time;
-        return Math.max(totalFare, MINIMUM_FARE);
-    }
+   public void setRideRepository(RideRepository rideRepository) {
+       this.rideRepository = rideRepository;
+   }
 
-    /** Calculating total fare for multiple rides*/
-    public InvoiceSummery calculateFare(Ride[] rides) {
+    public InvoiceSummery getTotalFare(Ride[] rides) {
         double totalFare = 0;
-        for(Ride ride:rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
-        }
-        return new InvoiceSummery(rides.length,totalFare);
+        for(Ride ride:rides)
+            totalFare += ride.cabRide.calculateCostOfRide(ride);
+        return new InvoiceSummery(rides.length, totalFare);
     }
 
     public void addRides(String userId, Ride[] rides) {
@@ -30,6 +20,6 @@ public class InvoiceService {
     }
 
     public InvoiceSummery getInvoiceSummery(String userId) {
-        return this.calculateFare(rideRepository.getRides(userId));
+        return this.getTotalFare(rideRepository.getRides(userId));
     }
 }
--- src/test/java/cabinvoice/InvoiceServiceTest.java
+++ src/test/java/cabinvoice/InvoiceServiceTest.java
@@ -6,47 +6,33 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
 
-    InvoiceService invoiceService;
+    private InvoiceService invoiceService;
+    private RideRepository rideRepository;
+    private  Ride[] rides = null;
+    InvoiceSummery expectedInvoiceSummery;
+
     @Before
-    public void setUp() {
+    public void initialize() {
         invoiceService=new InvoiceService();
+        rideRepository = new RideRepository();
+        invoiceService.setRideRepository(rideRepository);
+        rides = new Ride[] {
+                new Ride(CabRide.NORMAL,2.0,5),
+                new Ride(CabRide.PREMIUM,0.1,1)};
+        expectedInvoiceSummery = new InvoiceSummery(2,45.0);
     }
 
-    /** Given distance and time should return total fare*/
-    @Test
-    public void givenDistanceAndTime_shouldReturnTotalFare() {
-        double distance = 5.0;
-        int time = 5;
-        double fare = invoiceService.calculateFare(distance, time);
-        Assert.assertEquals(55.0, fare, 0.0);
-    }
-
-    /** Given distance and time, if total fare is less than minimum fare should return minimum fare*/
-    @Test
-    public void givenDistanceAndTime_WhenTotalFareLessThanMinimumFare_shouldReturnMinimumFare() {
-        double distance = 0.3;
-        int time = 1;
-        double fare = invoiceService.calculateFare(distance, time);
-        Assert.assertEquals(5.0, fare,0.0);
-    }
-
-    /** given distance and time of multiple rides should return multiple fare*/
     @Test
     public void givenMultipleRides_ShouldReturnInvoiceSummery() {
-        Ride[] rides = {new Ride(2.0,5), new Ride(0.1,1)};
-        InvoiceSummery invoiceSummery = invoiceService.calculateFare(rides);
-        InvoiceSummery expectedInvoiceSummery = new InvoiceSummery(2,30);
+        InvoiceSummery invoiceSummery = invoiceService.getTotalFare(rides);
         Assert.assertEquals(invoiceSummery,expectedInvoiceSummery);
     }
 
     @Test
     public void givenUserId_shouldReturnInvoiceSummery() {
         String userId = "priyanka1";
-        Ride[] rides = {new Ride(2.0,5),
-                new Ride(0.1,1) };
         invoiceService.addRides(userId,rides);
         InvoiceSummery invoiceSummery = invoiceService.getInvoiceSummery(userId);
-        InvoiceSummery invoiceSummeryExpected = new InvoiceSummery(2,30);
-        Assert.assertEquals(invoiceSummery,invoiceSummeryExpected);
+        Assert.assertEquals(invoiceSummery,expectedInvoiceSummery);
     }
 }
